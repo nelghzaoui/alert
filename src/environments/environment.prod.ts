481883a05cd65dd637462f06652bfc6de4c14a0e
Nasreddine <nasreddine@MacBook-Pro-de-Nasreddine.local>
2020-02-29 15:48:31 +0100

    align to new configuration for environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
-export const environment = {
-  production: true
-};
+import { Api, Environment, SslPinning } from '@core/models/environments';
+
+export const environment = new Environment('dev', 5, false, new Api('http://localhost:8080', SslPinning.NOCHECK));
+
+console.log(environment);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
+import { Api, Environment, SslPinning } from '@core/models/environments';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+export const environment = new Environment('dev', 5, false, new Api('http://localhost:8080', SslPinning.NOCHECK));
+
+console.log(environment);
 
 /*
  * For easier debugging in development mode, you can import the following file
