814bc9f4696493b2e708e1f92fd78843f219f6af
Nasreddine <nasreddine@MacBook-Pro-de-Nasreddine.local>
2020-03-09 23:47:28 +0100

    add hooks to copy resources

---
+++ scripts/android/copy-resources.js
@@ -0,0 +1,52 @@
+/*
+ * Hook set in config/config to run after prepare
+ *
+ * It copies files from ressources/ :
+ * - certificates to use with servers
+ * - properties for automatic signing
+ * - licence file for Ariadnext
+ * - build-extras to fix depedencies
+ */
+module.exports = ctx => {
+  const gulp = require('gulp');
+  const path = require('path');
+
+  console.log('CTX', ctx);
+
+  // Get Node Env
+  const env = process.env.NODE_ENV || 'dev';
+
+  console.log('Copy resources for: ' + env);
+
+  // Files to copy and paths
+  const src = path.join(ctx.opts.projectRoot, 'resources/android', env);
+  const files = [
+    {
+      name: `certificates/mobile-${env}.cer`,
+      dest: path.join(ctx.opts.projectRoot, 'www/certificates')
+    },
+    {
+      name: 'signing/**/*',
+      dest: path.join(ctx.opts.projectRoot, 'platforms/android')
+    },
+    {
+      name: 'build-extras.gradle',
+      dest: path.join(ctx.opts.projectRoot, 'platforms/android')
+    }
+  ];
+
+  // Copy files
+  files.forEach(file => {
+    try {
+      gulp
+        .src(path.join(src, file.name), {
+          allowEmpty: true
+        })
+        .pipe(gulp.dest(file.dest));
+    } catch (e) {
+      console.log(`Cannot copy certificate ${file.name}`);
+    }
+  });
+
+  console.log('---------------------------------------------------------------');
+};
---
+++ scripts/ios/copy-resources.js
@@ -0,0 +1,88 @@
+/*
+ * Hook set in config/config to run after prepare
+ *
+ * It copies files from ressources/ :
+ * - certificates to use with servers
+ * - properties for automatic signing
+ *
+ * Set "addResource: true" to copy files to Xcode projet
+ */
+module.exports = function(ctx) {
+  const fs = require('fs');
+  const gulp = require('gulp');
+  const xcode = require('xcode');
+  const path = require('path');
+
+  // Get Node Env
+  const env = process.env.NODE_ENV || 'dev';
+
+  console.log('Copy resources for: ' + env);
+
+  // Files to copy and paths
+  const src = path.join(ctx.opts.projectRoot, 'resources/ios', env);
+
+  const files = [
+    {
+      name: `certificates/mobile-${env}.cer`,
+      addResource: false,
+      dest: path.join(ctx.opts.projectRoot, 'www/certificates')
+    }
+  ];
+
+  // Recursively search for file with the tiven filter starting on startPath
+  function searchRecursiveFromPath(startPath, filter, rec, multiple) {
+    if (!fs.existsSync(startPath)) {
+      console.log('no dir ', startPath);
+      return;
+    }
+
+    const files = fs.readdirSync(startPath);
+    const resultFiles = [];
+    for (let i = 0; i < files.length; i++) {
+      const filename = path.join(startPath, files[i]);
+      const stat = fs.lstatSync(filename);
+      if (stat.isDirectory() && rec) {
+        fromDir(filename, filter); // recurse
+      }
+
+      if (filename.indexOf(filter) >= 0) {
+        if (multiple) {
+          resultFiles.push(filename);
+        } else {
+          return filename;
+        }
+      }
+    }
+    if (multiple) {
+      return resultFiles;
+    }
+  }
+
+  // Copy files
+  const xcodeProjPath = searchRecursiveFromPath('platforms/ios', '.xcodeproj', false);
+  const projectPath = xcodeProjPath + '/project.pbxproj';
+
+  const proj = xcode.project(projectPath);
+  proj.parse(() => {
+    files.forEach(file => {
+      if (!fs.existsSync(path.join(src, file.name))) {
+        return;
+      }
+
+      try {
+        gulp.src(path.join(src, file.name)).pipe(gulp.dest(file.dest));
+      } catch (e) {
+        console.log(`Cannot copy ${file.name}`);
+        return;
+      }
+
+      if (file.addResource) {
+        proj.addResourceFile(file.name);
+      }
+    });
+
+    fs.writeFileSync(projectPath, proj.writeSync());
+  });
+
+  console.log('---------------------------------------------------------------');
+};
