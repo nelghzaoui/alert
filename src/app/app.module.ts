5bae03588eaa59085a6221b8fdc001b5483747a0
Nasreddine <nasreddine@MacBook-Pro-de-Nasreddine.local>
2020-04-25 13:26:57 +0200

    register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { CoreModule } from '@core/core.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { CoreModule } from '@core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AccountModule } from '@account/account.module';
 import { ServerModule } from '@server/server.module';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'server', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'account',
+    pathMatch: 'full'
+  },
+  {
+    path: 'account',
+    loadChildren: (): Promise<typeof AccountModule> => import('@account/account.module').then((m) => m.AccountModule)
+  },
   {
     path: 'server',
     loadChildren: (): Promise<typeof ServerModule> => import('@server/server.module').then((m) => m.ServerModule)
